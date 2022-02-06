604c9223c9048ba56e20e9201cc8efb67cd1d217
Anastasia Emelyan <Nastusha0895@mail.ru>
2022-02-06 05:39:37 +0300

    fix:rename to MainSpringClass

--- src/main/java/com/emelyan/MainSpringClass.java
+++ src/main/java/com/emelyan/MainSpringClass.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainSpringApplication {
+public class MainSpringClass {
 	public static void main(String[] args) {
-		SpringApplication.run(MainSpringApplication.class, args);
+		SpringApplication.run(MainSpringClass.class, args);
 	}
 
 }
