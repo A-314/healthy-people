3d52ef8acbad31c0731996e89d5a6071f7a7f6ef
Anastasia Emelyan <Nastusha0895@mail.ru>
2022-04-22 14:11:03 +0300

    feat: add filter for patronymic find in DoctorRepository

--- src/main/java/com/emelyan/repositories/DoctorRepository.java
+++ src/main/java/com/emelyan/repositories/DoctorRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface DoctorRepository extends JpaRepository<Doctor,Long> {
     @Query(" select d from Doctor d where lower(d.person.name) like concat('%',lower(:filter),'%')" +
-            "or lower(d.person.surname) like concat('%',lower(:filter),'%')")
+            "or lower(d.person.surname) like concat('%',lower(:filter),'%')"+
+            "or lower(d.person.patronymic) like concat('%',lower(:filter),'%')")
     List<Doctor> findDoctorsWithFilter(String filter);
 }
