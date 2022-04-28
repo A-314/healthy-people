1b1a44dd8da047af6ca2b5ab070303093f254465
Anastasia Emelyan <Nastusha0895@mail.ru>
2022-04-28 18:57:44 +0300

    feat: add sort by "date" and "id" to list of Treatment

--- src/main/java/com/emelyan/services/TreatmentService.java
+++ src/main/java/com/emelyan/services/TreatmentService.java
@@ -2,6 +2,7 @@ package com.emelyan.services;
 
 import com.emelyan.models.Treatment;
 import com.emelyan.repositories.TreatmentRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +17,7 @@ public class TreatmentService {
 
     public void save(Treatment treatment){treatmentRepository.save(treatment);}
 
-    public List<Treatment> findAll(){return treatmentRepository.findAll();}
+    public List<Treatment> findAll(){return treatmentRepository.findAll(Sort.by("date","id"));}
 
     public Treatment getOne(Long id){return  treatmentRepository.getOne(id);}
 
